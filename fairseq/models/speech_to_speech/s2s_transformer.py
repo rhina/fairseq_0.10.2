d46990e08f775ff57cd1f486da8f169c6f3e56c0
Ryota Hinami <hinami@mantra.co.jp>
2025-04-19 14:51:47 +0900

    fixed conflicts dependency

--- fairseq/models/speech_to_speech/s2s_transformer.py
+++ fairseq/models/speech_to_speech/s2s_transformer.py
@@ -19,7 +19,8 @@ from fairseq.models import (
     register_model_architecture,
 )
 from fairseq.models.speech_to_text import S2TTransformerEncoder
-from fairseq.models.speech_to_speech.modules.ctc_decoder import CTCDecoder, StackedEmbedding
+from fairseq.models.speech_to_speech.modules.ctc_decoder import CTCDecoder
+from fairseq.models.speech_to_speech.modules.stacked_embedding import StackedEmbedding
 from fairseq.models.text_to_speech import TTSTransformerDecoder
 from fairseq.models.transformer import (
     Linear,
